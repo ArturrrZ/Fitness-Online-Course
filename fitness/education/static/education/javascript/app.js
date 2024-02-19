980dbf307d903e15f6e79d3d50d64b5ca87847a9
Artur Z <arturziianbaev@gmail.com>
2024-02-18 16:44:35 -0800

    CONNECTED REACT AND DJANGO APPLICATONS finally

---
+++ fitness/education/static/education/javascript/app.js
@@ -0,0 +1,16 @@
+
+// Функциональный компонент App
+function App() {
+
+    return (
+        <div>
+            <h1>Hello from REACT!</h1>
+        </div>
+    );
+}
+
+// Рендеринг компонента в элемент с id="root"
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
