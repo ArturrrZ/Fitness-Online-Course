61dd1be22c5f6ef9a0372f82e1fa5c2976b26b1d
Artur Z <arturziianbaev@gmail.com>
2024-02-22 11:28:39 -0800

    simple useState example

--- fitness/education/static/education/javascript/profile_teacher_view_new.js
+++ fitness/education/static/education/javascript/profile_teacher_view_new.js
@@ -1,3 +1,15 @@
+const { useState } = React;
+function Subcomponent(){
+    const [num,setNum] = useState(0);
+    function increase() {
+        setNum((prevNum) => prevNum + 1);
+      }    return (<div>
+    {num}
+    <button onClick={increase}>increase</button>
+    </div>)
+}
+
+
 const root=document.querySelector("#root");
 var personId=root.dataset.personid;
 fetch(`/api/get_person/${personId}`,{
@@ -12,12 +24,15 @@ fetch(`/api/get_person/${personId}`,{
   var person=data.person;
 //   we've got a USER PAGE!!!
   ReactDOM.render(<div>
+  <Subcomponent/>
     <h3>{person.headline}</h3>
     {freeContentArray.map(content=>{return (<h4 key={content.pk}>{content.fields.title}</h4>)})}
     <ul>
         {paidContentArray.map(content=>{
             return (
+                
                 <li key={content.pk}>
+                
                 <h3 >{content.fields.title}</h3>
                 <p>{content.fields.description}</p></li>    
             )
