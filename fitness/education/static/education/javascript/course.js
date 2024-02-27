014e0189be8598b12ee4cf3a2adabc0ba1c2fc77
Artur Z <arturziianbaev@gmail.com>
2024-02-27 09:43:30 -0800

    set state in js 1.0

--- fitness/education/static/education/javascript/course.js
+++ fitness/education/static/education/javascript/course.js
@@ -8,5 +8,32 @@ fetch(`/api/get_course/${courseId}`)
 .then(data=>{
     console.log(data);
     console.log(data["course"]["content"])
+
+    function App(props){
+        const [view,setView] = useState({
+            creator:props.data["course"]["creator"],
+            creator_image_url:props.data["course"]["creator_image_url"],
+            name:props.data["course"]["name"],
+            overview:props.data["course"]["overview"],
+            url_image:props.data["course"]["url_image"],
+            // changeable part
+            video:props.data["course"]["content"][0].url_youtube,
+            description:props.data["course"]["content"][0].description
+        })
+        console.log(view);
+        return (<p>{props.data["course"]["name"]}s</p>)
+    }
+    
+
+
+
+
+
+
+
+
+
+    ReactDOM.render(<div>
+        <App data={data}/>
+    </div>,root);
 })
-ReactDOM.render(<p>Hey {courseId}</p>,root);
