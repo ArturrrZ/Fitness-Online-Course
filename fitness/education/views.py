af421bd1380f41d3636c6f898ee1f2157255c617
Artur Z <arturziianbaev@gmail.com>
2024-03-21 17:33:05 -0700

    finish edit feature 2.0

--- fitness/education/views.py
+++ fitness/education/views.py
@@ -780,6 +780,10 @@ def get_course(request,course_id):
             if request.user.is_authenticated:
                 if rating['user__username'] == request.user.username:
                     is_rated = True
+                    rating["rated_course"] = True
+                else:
+                    # is_rated=False
+                    rating["rated_course"] = False
             else:
                 pass
 
